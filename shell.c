ec41f699fa60d09c4b256cf218ca552b4ed23db9
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-12-08 13:51:40 -0800

    Resolved all memory leaks

--- shell.c
+++ shell.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
-int main(int ac, char **av, char **environ)
+int main(__attribute__((unused))int ac, char **av, char **environ)
 {
 	char *command = NULL, **args = NULL;
-	size_t len = 0, command_counter = 1;
+	size_t len, command_counter = 1;
 	ssize_t command_length = 0;
 	struct stat command_info;
 	int is_terminal = 0;
@@ -13,6 +13,7 @@ int main(int ac, char **av, char **environ)
 	while(1)
 	{
 		printf("$ ");
+		len = 0;
 		command_length = getline(&command, &len, stdin);
 		if (command_length == -1)
 			break;
@@ -21,28 +22,32 @@ int main(int ac, char **av, char **environ)
 		{
 			printf("%s: %ld: %s\n", av[0], command_counter, command);
 			command_counter++;
+			free(command);
 			continue;
 		}
-		args = (char **)malloc(sizeof(char *) * 1);
+		args = (char **)malloc(sizeof(char *) * 2);
 		if (args == NULL)
 		{
 			free(command);
 			break;
 		}
-		args[0] = custom_strdup(command);
+		args[0] = command;
 		args[1] = NULL;
 		if (execute(args, environ) == -1)
 		{
 			printf("%s: %ld: %s\n", av[0], command_counter, command);
 			command_counter++;
+			free(args[0]);
+			free(args);
 			continue;
 		}
 		if (is_terminal == 1)
 			break;
 		command_counter++;
+		free(args[0]);
+		free(args);
 
 	}
-	free(command);
 	free(args[0]);
 	free(args);
 	return (0);
