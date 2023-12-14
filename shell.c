a992b24d622c5bd8d6a8a9443227466636144e78
Oluchukwu Joseph <devjoseph016@gmail.com>
2023-12-14 19:18:16 +0100

    Fixed errors in Task 1

--- shell.c
+++ shell.c
@@ -13,10 +13,7 @@ int main(__attribute__((unused))int ac, char **av, char **environ)
 	char *command = NULL, **args = NULL;
 	size_t len, command_counter = 1;
 	ssize_t command_length = 0;
-	int is_terminal = 0;
 
-	if (!isatty(fileno(stdin)))
-		is_terminal = 1;
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -62,8 +59,6 @@ int main(__attribute__((unused))int ac, char **av, char **environ)
 		free(command);
 		command = NULL;
 		free_double_pointer(args);
-		if (is_terminal == 1)
-			exit(EXIT_SUCCESS);
 		command_counter++;
 	}
 	return (0);
